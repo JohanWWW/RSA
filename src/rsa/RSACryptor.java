933936049af6a0a103c3ad19742ea073dcd970d1
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2021-01-22 15:21:56 +0100

    add method overloads for encrypting and decrypting strings

--- src/rsa/RSACryptor.java
+++ src/rsa/RSACryptor.java
@@ -1,6 +1,7 @@
 package rsa;
 
 import java.math.BigInteger;
+import java.nio.charset.Charset;
 
 public class RSACryptor {
 
@@ -8,7 +9,16 @@ public class RSACryptor {
         return (new BigInteger(message)).modPow(key.getKey(), key.getN()).toByteArray();
     }
 
+    public static String encrypt(String message, RSAKey key) {
+        return (new BigInteger(message.getBytes())).modPow(key.getKey(), key.getN()).toString();
+    }
+
     public static byte[] decrypt(byte[] message, RSAKey key) {
         return (new BigInteger(message)).modPow(key.getKey(), key.getN()).toByteArray();
+    }
+
+    public static String decrypt(String message, Charset charset, RSAKey key) {
+        String s = new String(message.getBytes(charset));
+        return new String((new BigInteger(s)).modPow(key.getKey(), key.getN()).toByteArray());
     }
 }
