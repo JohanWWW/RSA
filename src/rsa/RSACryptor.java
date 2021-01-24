82c9077c19019ca26eaaa597481f709a30398ed4
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2021-01-24 17:10:50 +0100

    base64 encode results

--- src/rsa/RSACryptor.java
+++ src/rsa/RSACryptor.java
@@ -2,23 +2,28 @@ package rsa;
 
 import java.math.BigInteger;
 import java.nio.charset.Charset;
+import java.util.Base64;
 
 public class RSACryptor {
 
     public static byte[] encrypt(byte[] message, RSAKey key) {
-        return (new BigInteger(message)).modPow(key.getKey(), key.getN()).toByteArray();
+        byte[] encryptedData = (new BigInteger(message)).modPow(key.getKey(), key.getN()).toByteArray();
+        return Base64.getEncoder().encode(encryptedData);
     }
 
     public static String encrypt(String message, RSAKey key) {
-        return (new BigInteger(message.getBytes())).modPow(key.getKey(), key.getN()).toString();
+        String encryptedString = (new BigInteger(message.getBytes())).modPow(key.getKey(), key.getN()).toString();
+        return Base64.getEncoder().encodeToString(encryptedString.getBytes());
     }
 
     public static byte[] decrypt(byte[] message, RSAKey key) {
-        return (new BigInteger(message)).modPow(key.getKey(), key.getN()).toByteArray();
+        byte[] decodedData = Base64.getDecoder().decode(message);
+        return (new BigInteger(decodedData)).modPow(key.getKey(), key.getN()).toByteArray();
     }
 
     public static String decrypt(String message, Charset charset, RSAKey key) {
-        String s = new String(message.getBytes(charset));
+        String decodedString = new String(Base64.getDecoder().decode(message));
+        String s = new String(decodedString.getBytes(charset));
         return new String((new BigInteger(s)).modPow(key.getKey(), key.getN()).toByteArray());
     }
 }
